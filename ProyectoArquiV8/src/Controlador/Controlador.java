328d61e16013b47760921d54b46f7f2b1c323184
Alex Altuve <141680817+Alex-Altuve@users.noreply.github.com>
2023-08-11 15:06:32 -0400

    Add files via upload

---
+++ ProyectoArquiV8/src/Controlador/Controlador.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author Alex Altuve
+ */
+public class Controlador {
+   
+    
+    public void iniciaVentana(JFrame ventana, String ruta) {
+        ventana.setLocationRelativeTo(null);
+        ventana.setIconImage(new ImageIcon(ruta).getImage());
+        ventana.setResizable(false);
+    }
+    public void activaVentana(JFrame ventana,JFrame ventana2) {
+        
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+        ventana2.dispose(); 
+    }
+    
+}
